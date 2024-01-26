6759a5f3961af4a5fa55a93f08ae628857c1d59e
ohtaekwon <ohtk1119@gmail.com>
2024-01-26 17:06:10 +0900

    [MODIFY] 불필요한 코드 제거

--- index.ts
+++ index.ts
@@ -4,4 +4,3 @@ export { default as useCopyClipBoard } from "./src/hooks/useCopyClipBoard";
 export { default as usePageTitle } from "./src/hooks/usePageTitle";
 export { default as useMultiState } from "./src/hooks/useMultiState";
 export { default as useWindowResize } from "./src/hooks/useWindowResize";
-export { default as useWindowResize } from "./src/hooks/useWindowResize";
