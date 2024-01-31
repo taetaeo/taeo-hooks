4c1b459d3f9bef88ec5781a3e1e1af6bcb9db3fe
ohtaekwon <ohtk1119@gmail.com>
2024-01-31 10:19:15 +0900

    [UPDATE] 로컬스토리지 관리하는 훅에 기능 추가(삭제, 업데이트, 저장)

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -3,69 +3,108 @@ import { useCallback, useEffect, useState } from "react";
 
 type SetValue<T> = Dispatch<SetStateAction<T>>;
 
+interface Options {
+  initialSave: boolean;
+}
+
 const isServer = typeof window === "undefined";
 
-export default function useLocalStorage<T>(key: string, initialValue: T | (() => T), options = {}) {
+/**
+ * 이 훅은 로컬스토리지를 관리하는 커스텀 훅 입니다.
+ * 기본적으로 로컬스토리지의 값을 읽기, 저장, 삭제, 업데이트 등의 기능을 제공하고 있습니다.
+ * @param {string} key - 로컬 스토리지의 Key
+ * @param {T} initialValue - 로컬 스토리지의 초깃값
+ * @param {Options} options - 훅에서 쓰이는 옵션 값
+ * @returns {()=>{}} read(읽기), save(저장), remove(삭제), update(업데이트 마운트)
+ */
+export default function useLocalStorage<T>(key: string, initialValue: T | (() => T), options: Options = { initialSave: false }) {
   const [storedValue, setStoredValue] = useState<T>(initialValue);
 
+  const isStorageEvent = (event: StorageEvent | CustomEvent): event is StorageEvent => {
+    return "key" in event;
+  };
+
   /** 로컬 스토리지 읽기 */
-  const read = () =>
-    useCallback((): T => {
-      // initialValue 가 함수인지 아닌지 여부 확인한다. true = 함수형일 경우 해당 함수를 호출한다. / false = 함수형이 아닐 경우 초깃값을 그대로 호출한다.
-      const initialValueToUse = initialValue instanceof Function ? initialValue() : initialValue;
+  const readValueFromLocalStorage = (): T => {
+    const initialValueToUse = initialValue instanceof Function ? initialValue() : initialValue;
 
-      // SSR에서 발생할 수 있는 오류 방지하기 위해 설정. window 객체가 존재하지 않을때, localStorage에 접근 하는 것을 방지
-      if (isServer) {
-        return initialValueToUse;
-      }
-
-      try {
-        const rawData = window.localStorage.getItem(key) as T;
-
-        if (!rawData) return initialValueToUse;
-
-        return rawData ? rawData : initialValueToUse;
-      } catch {
-        console.warn(`로컬 스토리지를 가져오는 것에 실패하였습니다. 키 : "${key}", 값 : error  `);
-        return initialValueToUse;
-      }
-    }, [initialValue, key]);
-
-  /** 로컬 스토리지 저장 */
-  const save: SetValue<T> = useCallback((value) => {
+    // SSR에서 발생할 수 있는 오류 방지하기 위해 설정. window 객체가 존재하지 않을때, localStorage에 접근 하는 것을 방지
     if (isServer) {
-      console.warn("브라우저 환경에서만 사용 가능합니다. 서버환경에서 사용이 불가능합니다.");
+      return initialValueToUse;
     }
-    try {
-      setStoredValue(value);
 
-      window.localStorage.setItem(key, JSON.stringify(value));
+    try {
+      const rawData = window.localStorage.getItem(key) as T;
+
+      if (!rawData) return initialValueToUse;
+
+      return rawData ? rawData : initialValueToUse;
     } catch {
-      return;
+      console.warn(`로컬 스토리지를 가져오는 것에 실패하였습니다. 키 : "${key}", 값 : error  `);
+      return initialValueToUse;
     }
+  };
+  // read함수 메모이제이션
+  const read = useCallback<() => T>(() => {
+    return readValueFromLocalStorage();
   }, []);
 
   useEffect(() => {
     setStoredValue(read());
   }, [key]);
 
-  /** 로컬 스토리지 수정 */
+  /** 로컬 스토리지 읽기 */
+
+  /** 로컬 스토리지 저장 */
+  const saveValueFromLocalStorage = (value: T) => {
+    if (isServer) {
+      console.warn("브라우저 환경에서만 사용 가능합니다. 서버환경에서 사용이 불가능합니다.");
+      return undefined;
+    }
+
+    try {
+      setStoredValue(value);
+      window.localStorage.setItem(key, JSON.stringify(value));
+      return value;
+    } catch (error) {
+      if (error instanceof DOMException && error.name === "QuotaExceededError") {
+        console.error("실패 : 로컬 스토리지 용량이 부족합니다.");
+      } else {
+        console.warn(`로컬 스토리지에 값을 저장하는 데 실패했습니다. 키: "${key}"`);
+      }
+    }
+  };
+
+  const save: SetValue<T> = useCallback(async (value: T) => {
+    return await saveValueFromLocalStorage(value);
+  }, []);
+  /** 로컬 스토리지 저장 */
+
+  /** 로컬 스토리지 변화에 따른 상태관리 감지 */
   const update = useCallback(
     (event: StorageEvent | CustomEvent) => {
-      if ((event as StorageEvent)?.key && (event as StorageEvent).key !== key) {
-        return;
-      }
+      if (isStorageEvent(event) && event.key !== key) return;
       setStoredValue(read());
     },
     [key, read]
   );
+  /** 로컬 스토리지 변화에 따른 상태관리 감지 */
 
   /** 로컬 스토리지 삭제 */
-  const remove = () => {};
+  const remove = () => {
+    if (isServer) {
+      console.warn("현재 브라우저 환경이 아닙니다.");
+      return key;
+    }
 
-  useEffect(() => {
-    setStoredValue(read());
-  }, [key]);
+    try {
+      window.localStorage.removeItem(key);
+      setStoredValue(undefined);
+    } catch (error) {
+      console.warn(`로컬 스토리지에서 항목을 삭제하는 데 실패했습니다. 키: "${key}"`);
+      return;
+    }
+  };
 
-  return { read, update };
+  return { read, save, remove, update };
 }
