e45c6f04ffa4519af2eab30e54150fcf8c39788b
taeo <ohtk1119@gmail.com>
2024-09-06 14:30:31 +0900

    [UPDATE] 여러개의 ref에 대해서 대응이가능하도록 수정

--- src/hooks/useOutSideClick.ts
+++ src/hooks/useOutSideClick.ts
@@ -7,7 +7,7 @@ import type { RefObject } from "react";
  * @param {RefObject<HTMLElement>} ref - 외부 클릭을 감지할 대상 요소의 Ref 객체
  * @param {()=>void} callback - 외부 클릭이 감지되었을 때, 호출될 콜백 함수
  */
-export default function useOutSideClick(ref: RefObject<HTMLElement>, callback: () => void) {
+export default function useOutSideClick(ref: RefObject<HTMLElement>, callback: () => void, refs?: RefObject<HTMLElement>[]) {
   useEffect(() => {
     /**
      *
@@ -16,8 +16,14 @@ export default function useOutSideClick(ref: RefObject<HTMLElement>, callback: (
      * @param {MouseEvent} event - mousedown 이벤트 객체
      */
     const handleClick = (event: MouseEvent) => {
+      // 추가적인 ref들이 존재할 경우, 이 ref들 중 하나라도 클릭된 경우 콜백 호출을 방지
+      if (refs && refs.some((exceptRef) => exceptRef.current?.contains(event.target as Node))) {
+        return;
+      }
+
+      // 기본 ref가 유효하고 클릭된 요소가 이 ref의 자식이 아닌 경우에만 콜백 호출
       if (ref.current && !ref.current.contains(event.target as Node)) {
-        callback?.();
+        callback();
       }
     };
     // mousedown 이벤트에 대한 이벤트 리스너 등록
