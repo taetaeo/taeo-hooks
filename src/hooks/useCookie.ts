3d4e2424bce77497af843972b9e98ac78acda8e2
taeo <ohtk1119@gmail.com>
2024-09-06 14:29:22 +0900

    [UPDATE] Retunr type Change : Array -> Object

--- src/hooks/useCookie.ts
+++ src/hooks/useCookie.ts
@@ -1,17 +1,17 @@
 import { useCallback, useState } from "react";
 import Cookies from "js-cookie";
 
-type UseCookieReturnType = [string | null, (newValue: string, options?: Cookies.CookieAttributes) => void, () => void];
-
 /**
  * 쿠키를 관리하기 위한 커스텀 React 훅입니다.
  *
  * @param {string} cookieKey - 쿠키의 키 또는 이름입니다.
  * @returns {UseCookieReturnType} 현재 쿠키 값, 쿠키를 업데이트하는 함수, 쿠키를 삭제하는 함수를 담은 배열을 반환합니다.
  */
-export default function useCookie(cookieKey: string): UseCookieReturnType {
+export type UseCookieReturnType = ReturnType<typeof useCookie>;
+
+export default function useCookie(cookieKey: string) {
   // 쿠키의 현재 값 보관을 위한 상태 변수
-  const [value, setValue] = useState<string | null>(() => Cookies.get(cookieKey) || null);
+  const [cookie, setCookie] = useState<string | null>(() => Cookies.get(cookieKey) || null);
 
   /**
    * @event UPDATE
@@ -23,7 +23,7 @@ export default function useCookie(cookieKey: string): UseCookieReturnType {
   const updateCookie = useCallback(
     (newValue: string, options?: Cookies.CookieAttributes) => {
       Cookies.set(cookieKey, newValue, options);
-      setValue(newValue);
+      setCookie(newValue);
     },
     [cookieKey]
   );
@@ -34,9 +34,9 @@ export default function useCookie(cookieKey: string): UseCookieReturnType {
    */
   const deleteCookie = useCallback(() => {
     Cookies.remove(cookieKey);
-    setValue(null);
+    setCookie(null);
   }, [cookieKey]);
 
-  // 현재 값, 업데이트 함수, 삭제 함수를 담은 배열을 반환합니다.
-  return [value, updateCookie, deleteCookie];
+  // 현재 값, 업데이트 함수, 삭제 함수를 담은 객체를 반환합니다.
+  return { cookie, updateCookie, deleteCookie };
 }
