a00621ee1e5b968adc743ed5cbd821237747c9ef
taeo <ohtk1119@gmail.com>
2024-09-09 09:11:13 +0900

    [ADD] Fetch 기능의 훅 추가

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+
+export interface ReturnFetch<T> {
+  data: T | null;
+  loading: boolean;
+  error: Error | null;
+}
+
+export interface FetchOptions extends RequestInit {
+  // 추가 옵션
+}
+
+const useFetch = <T>(url: string, options?: FetchOptions): ReturnFetch<T> => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(url, {
+          method: "get",
+          ...options,
+          headers: {
+            ...options?.headers,
+          },
+
+          signal: controller.signal,
+        });
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+        const result: T = await response.json();
+        setData(result);
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          setError(err as Error);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, [url, options]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
