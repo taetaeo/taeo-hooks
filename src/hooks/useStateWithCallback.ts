594eea31724d23c74031f79de87be144a77f37ec
taeo <ohtk1119@gmail.com>
2024-09-12 11:02:17 +0900

    [FEAT] 동기처리하기 위한 훅 :Wq ;wq

---
+++ src/hooks/useStateWithCallback.ts
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+export type UseStateWithCallback<T> = [T, (newState: T, callback?: (state: T) => void) => void];
+
+export default function useStateWithCallback<T>(initialValue: T): UseStateWithCallback<T> {
+  const [state, setState] = React.useState<T>(initialValue);
+  const callbackRef = React.useRef<((state: T) => void) | null>(null);
+
+  const setStateWithCallback = React.useCallback((newState: T, callback?: (state: T) => void) => {
+    callbackRef.current = callback || null;
+    setState(newState);
+  }, []);
+
+  React.useEffect(() => {
+    if (callbackRef.current) {
+      callbackRef.current(state);
+      callbackRef.current = null;
+    }
+  }, [state]);
+
+  return [state, setStateWithCallback];
+}
